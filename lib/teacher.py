5c3e7813ff38e5c673cde8119f781c3313ab8904
tylersmith <tyjsmith1@gmail.com>
2023-12-14 11:09:50 -0600

    done

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,17 @@ import random
 
 class Teacher(User):
 
+    knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+    ]
+    
     def teach(self):
-        pass
+        random_index = random.randint(0,len(self.knowledge)-1)
+        return self.knowledge[random_index]
